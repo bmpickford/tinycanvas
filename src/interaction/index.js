00cbd8ae902b7d08b6b21195586452f8cabefb42
Benjamin Pickford <b.mpickford@outlook.com>
2021-11-20 11:41:09 +1000

    puzzle

--- src/interaction/index.js
+++ src/interaction/index.js
@@ -1,15 +1,15 @@
-import { ArrowMoveMixin } from "./ArrowMoveMixin";
-import { WASDMoveMixin } from "./WASDMoveMixin";
-import { canvas } from "../canvas";
+import { arrowKeyListenerMixin } from "./mixins/arrowKeyListener";
+import { wasdKeyListenerMixin } from "./mixins/wasdKeyListener";
+import { movementMixin } from "./mixins/movement";
 
 export const withArrowMovement = (obj) => {
-    const _obj = withMixin(ArrowMoveMixin, obj);
+    const _obj = withMixin(arrowKeyListenerMixin, obj);
     _obj.init();
     return _obj;
 };
 
 export const withWASDMovement = (obj) => {
-    const _obj = withMixin(WASDMoveMixin, obj);
+    const _obj = withMixin(wasdKeyListenerMixin, obj);
     _obj.init();
     return _obj;
 };
@@ -20,38 +20,4 @@ const withMixin = (mixin, obj) => {
     Object.assign(_o.__proto__, movementMixin);
 
     return _o;
-}
-
-const movementMixin = {
-    init() {
-        this.boundOnKeyDown = this.onKeyDown.bind(this);
-        document.addEventListener('keydown', this.boundOnKeyDown);
-    },
-    destroy() {
-        document.removeEventListener('keydown', this.boundOnKeyDown);
-    },
-    moveLeft() {
-        if (this.x - this.delta < 0){ 
-            return;
-        }
-        this.x -= this.delta;
-    },
-    moveUp() {
-        if (this.y - this.delta < 0){ 
-            return;
-        }
-        this.y -= this.delta;
-    },
-    moveRight() {
-        if (this.x + (this.size * 2) + this.delta > canvas.width){ 
-            return;
-        }
-        this.x += this.delta;
-    },
-    moveDown() {
-        if (this.y + (this.size * 2) + this.delta > canvas.width){ 
-            return;
-        }
-        this.y += this.delta;
-    },
 }
---
+++ src/interaction/mixins/movement.js
@@ -0,0 +1,35 @@
+import { canvas } from "../../canvas";
+
+export const movementMixin = {
+    init() {
+        this.boundOnKeyDown = this.onKeyDown.bind(this);
+        document.addEventListener('keydown', this.boundOnKeyDown);
+    },
+    destroy() {
+        document.removeEventListener('keydown', this.boundOnKeyDown);
+    },
+    moveLeft() {
+        if (this.x - this.delta < 0){ 
+            return;
+        }
+        this.x -= this.delta;
+    },
+    moveUp() {
+        if (this.y - this.delta < 0){ 
+            return;
+        }
+        this.y -= this.delta;
+    },
+    moveRight() {
+        if (this.x + (this.size * 2) + this.delta > canvas.width){ 
+            return;
+        }
+        this.x += this.delta;
+    },
+    moveDown() {
+        if (this.y + (this.size * 2) + this.delta > canvas.width){ 
+            return;
+        }
+        this.y += this.delta;
+    },
+}

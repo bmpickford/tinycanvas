15a051cb257cc4ddfc8fb18bb4b4717d5501a036
Benjamin Pickford <b.mpickford@outlook.com>
2021-12-31 11:29:20 +1000

    dont minify example

--- example/rollup.config.js
+++ example/rollup.config.js
@@ -1,6 +1,5 @@
 import html from '@web/rollup-plugin-html';
 import summary from 'rollup-plugin-bundle-summary';
-import { terser } from 'rollup-plugin-terser';
 import copy from 'rollup-plugin-copy';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 
@@ -8,14 +7,7 @@ export default {
   input: 'index.html',
   output: { dir: 'dist' },
   plugins: [
-    html({ minify: true }),
-    terser({
-      mangle: {
-        properties: {
-          regex: /^_/,
-        },
-      },
-    }),
+    html(),
     summary(),
     copy({
       targets: [
